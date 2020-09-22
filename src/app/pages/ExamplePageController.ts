b4d11aed5406aaf8ae0e1a4ef26ad4b76aa5c72b
camiloruiz27 <camiloruiz27@hotmail.com>
2020-09-21 21:27:08 -0500

    realizado el 21 sep

--- src/app/pages/ExamplePageController.ts
+++ src/app/pages/ExamplePageController.ts
@@ -1,4 +1,4 @@
-import { before } from "lodash";
+import { before, functionsIn } from "lodash";
 
 export class ExamplePageController {
     view: any;
@@ -6,6 +6,7 @@ export class ExamplePageController {
         id: 'example-page',
         view: 'ExamplePage.html',
         style: 'ExamplePage.scss',
+        //cargito:()=>{this.cargito();},
         start: () => { this.start(); }
     }
 
@@ -15,6 +16,23 @@ export class ExamplePageController {
     }
     
     start() {
+        //window.scrollTo(0,0);
+        //window.play
+    const loader: any = this.findInsideMe(".loader");
+    const container: any = this.findInsideMe(".container");
+    const TopBarColor: any = this.findInsideMe(".Topbar");
+    const TextBien: any = this.findInsideMe(".Titulo");
+    console.log("Entre");
+    
+    setTimeout(function () {
+    TopBarColor.style.opacity = 1;
+      TextBien.style.opacity = 1; 
+      console.log("Entre en time");
+      loader.style.opacity = 0;
+      container.style.opacity = 0;      
+      
+    }, 3000);
+
         const box: any = this.findInsideMe(".box");
         const boxi: any = this.findInsideMe(".box2");
         const TDBox: any = this.findInsideMe(".TDBox");
@@ -22,18 +40,44 @@ export class ExamplePageController {
         const contenedor: any =this.findInsideMe(".contenedorgrande");
         const clouds: any = this.findInsideMe(".cloud", true);
         const seccion2: any =this.findInsideMe(".Seccion2");
+        const a:any=this.findInsideMe("a");
         const linea2: any =this.findInsideMe(".linea2");
+        
         window.onscroll = (_:any) => {
             console.log(`X: ${window.scrollX} Y: ${window.scrollY}`)
+            if (window.scrollY <= 2400) {
+            TopBarColor.style.background = 'rgb(102,147,74)';
+            a.stye.color='rgb(255,255,255)';
+            }
+            if (window.scrollY >= 2400) {
+            TopBarColor.style.background = 'rgb(232, 232, 112)';
+            }
+            if (window.scrollY >= 5050) {
+                //Azul Guaracha
+            TopBarColor.style.background = '#4581ac';
+            }
+            if (window.scrollY >= 10000) {
+            TopBarColor.style.background = ' #6a8a8a';
+            }
+            if (window.scrollY >= 13000) {
+            TopBarColor.style.background = ' rgb(137, 208, 141)';
+            }
+            if (window.scrollY >= 20800) {
+            TopBarColor.style.background = ' rgb(168, 189, 31)';
+            }
+            if (window.scrollY >= 22500) {
+            TopBarColor.style.background = 'rgb(232, 232, 112)';
+            }
+            
             const p = (window.scrollY / window.innerWidth) * 100;
             const j = (window.scrollY /2499)*60;
             console.log(`valor j ${j}`)
             //const scrollDerecha = (contenedor.scrollTop/ contenedor.clientWidth) * 100;
             let numero=window.scrollY
             
-            window.moveBy(numero,0 );
             
             if (window.scrollY >= 2399) {
+                
                 if (linea2.style.width ==`${60}`) {
                     linea2.style.width=`${60}%`
                     
@@ -96,9 +140,5 @@ export class ExamplePageController {
     getView(): [string, DocumentFragment] {
         return [this.component.id, document.createRange().createContextualFragment(this.view)];
     }
-
-    getTrainingName(n:number){
-        // button click handler
-     }
 
 }
